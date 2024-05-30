1fd512d85403b68cc0659a0fcd40b0bc18efbcb5
GUSTAVO PAZ <gustavo.paz@portalsesisp.org.br>
2024-05-30 14:12:35 -0300

    Exemplos atualizados

--- app.py
+++ app.py
@@ -1,17 +1,20 @@
 from flask import Flask, render_template, request, redirect
 from flask_migrate import Migrate
 from model import db, Usuario
-from gemini import Gemini_Connect
-from cep import obter_info_cep
+from gemini import Gemini
+from cep import CEPService
 from formatar import formatar_texto
 import re
 
+# Criando um app Flask e Configurando o SQL_Alchemy para usar o banco SQLite
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///usuarios.db'
 
+# Utilizando migrate para o versionamento de Banco de Dados
 db.init_app(app)
 migrate = Migrate(app, db)
 
+# ROTA DE CADASTRO
 @app.get('/')
 @app.get('/cadastro')
 def index_get():
@@ -21,25 +24,28 @@ def index_get():
 # CREATE
 @app.post('/prompt')
 def index_post():
-  chat = Gemini_Connect() # Criando o chat, que faz conexão com a API do Gemini
-  nome = request.form.get('nome')  # Entrada
-  cep = request.form.get('cep') # Entrada
-  prompt = request.form.get('prompt') # Entrada
-  info_cep = obter_info_cep(cep) # Realiza a requisição do CEP
-  if not cep:  # Verificar se o campo do CEP não está vazio
-      info_cep = ""  # Definir o endereço como uma string vazia
-  else:
-      info_cep = obter_info_cep(cep)
-      if not info_cep:
-        return render_template('erro.html')
-  response = chat.send_message(f'{prompt},{info_cep.values()}') # Envio da mensagem para o Gemini
-  saida = response.text.replace('*','') # Saída
+  gemini_instance = Gemini.get_instance() # Criando uma Instância da classe Gemini
+  chat = gemini_instance.start_chat() # Criando o chat que faz conexão com a API do Gemini
+  
+  nome = request.form.get('nome')  # Recebendo o nome na Entrada
+  cep = request.form.get('cep') # Recebendo o CEP na Entrada
+  prompt = request.form.get('prompt') # Recebendo o Prompt/Relato na Entrada
+  
+  if cep and not CEPService.is_valid_cep(cep): return render_template('erro.html') # Retornar um Erro caso a entrada seja inválida
+  info_cep = CEPService.obter_info_cep(cep) if cep else None # Verificando se o usuário inseriu algo que não seja no formato de um CEP
+  if info_cep: mensagem = f'{prompt}, {info_cep.values()}'# Preparando a mensagem que vai para o prompt
+  else: mensagem = prompt # Se não tiver informações do CEP, enviaremos apenas o relato
+  
+  response = chat.send_message(mensagem) # Envio da mensagem para o Gemini
+  saida = response.text.replace('*','') # Removendo todas as ocorrências de * (Utilizado para deixar em negrito)
   prioridade = re.findall(r'Prioridade:\s*(.*)', saida)[0] # Regex para filtrar a primeira ocorrência de Prioridade
+  
   usuario = Usuario(nome, cep, prompt, saida, prioridade.replace("</strong>", "")) # Criação de uma instância de Usuario 
   db.session.add(usuario) # Adicionando o usuario criado no Banco SQLite 
-  db.session.commit() # Confirma o comando 
-  saida = formatar_texto(saida)
-  return render_template('saida.html', usuario=usuario, saida=saida)  # Exibe tela com a saída
+  db.session.commit() # Confirmando o comando 
+  
+  saida = formatar_texto(saida) # Função que formata a saída do Gemini para ser exibida de uma forma mais agradável
+  return render_template('saida.html', usuario=usuario, saida=saida)  # Exibindo a tela com a saída
 
 
 # READ
@@ -49,6 +55,7 @@ def resgates_get():
   return render_template('resgates.html', usuarios = usuarios)
 
 
+# ROTA DE PERFIL
 @app.get('/perfil/<int:id>')
 def exibir_perfil(id):
    usuario = Usuario.query.get(id)
@@ -75,11 +82,10 @@ def deletar(id):
   return redirect('/resgates')
 
 
+# ERROR
 @app.errorhandler(404)
 def page_not_found(e):
     return render_template('erro.html')
 
 if __name__ == '__main__':
-  # with app.app_context():
-  #     db.create_all()
   app.run(debug=True)
--- cep.py
+++ cep.py
@@ -1,15 +1,30 @@
 import requests
-def obter_info_cep(cep):
-  response = requests.get(f'https://viacep.com.br/ws/{cep}/json/')
-  if response.status_code == 200:
-      data = response.json()
-      endereco = {
-          "logradouro": data.get("logradouro", ""),
-          "complemento": data.get("complemento", ""),
-          "bairro": data.get("bairro", ""),
-          "localidade": data.get("localidade", ""),
-          "uf": data.get("uf", "")
-      }
-      return endereco
-  else:
-      return None
+import re
+
+class CEPService:
+    BASE_URL = 'https://viacep.com.br/ws'
+
+    @staticmethod
+    def obter_info_cep(cep):
+        if not CEPService.is_valid_cep(cep):
+            return None
+        
+        url = f'{CEPService.BASE_URL}/{cep}/json/'
+        response = requests.get(url)
+        if response.status_code == 200:
+            data = response.json()
+            if 'erro' in data:
+                return None
+            endereco = {
+                "logradouro": data.get("logradouro", ""),
+                "complemento": data.get("complemento", ""),
+                "bairro": data.get("bairro", ""),
+                "localidade": data.get("localidade", ""),
+                "uf": data.get("uf", "")
+            }
+            return endereco
+        return None
+
+    @staticmethod
+    def is_valid_cep(cep):
+        return re.fullmatch(r'\d{8}|\d{5}-\d{3}', cep) is not None
